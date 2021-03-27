ef63b9dab487939da7010f35f35af12d20e844da
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-03-27 23:03:16 +0700

    install react navigation + basic setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {GetStarted, Splash} from './pages';
+import {NavigationContainer} from '@react-navigation/native';
 
 function App() {
-  return <GetStarted />;
+  return (
+    <NavigationContainer>
+      <GetStarted />
+    </NavigationContainer>
+  );
 }
 
 export default App;
