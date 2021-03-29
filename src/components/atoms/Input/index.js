a1fff8054f9bc233982b0d5122b8378f3de84fc1
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-03-29 17:29:52 +0700

    slicing halaman register dan create global colors

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 
+import {colors} from '../../../utils';
+
 const Input = ({label}) => {
   return (
     <View>
@@ -15,7 +17,7 @@ export default Input;
 const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
-    borderColor: '#E9E9E9',
+    borderColor: colors.border,
     borderRadius: 10,
     padding: 12,
   },
@@ -23,6 +25,6 @@ const styles = StyleSheet.create({
     marginBottom: 6,
     fontFamily: 'Nunito-Regular',
     fontSize: 16,
-    color: '#7D8797',
+    color: colors.text.secondary,
   },
 });
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Button, Gap, Header, Input} from '../../components';
+import {colors} from '../../utils';
 
 const Register = () => {
   return (
-    <View>
-      <Text>Page Register</Text>
+    <View style={styles.page}>
+      <Header />
+      <View style={styles.content}>
+        <Input label="Full Name" />
+        <Gap height={24} />
+        <Input label="Pekerjaan" />
+        <Gap height={24} />
+        <Input label="Email Address" />
+        <Gap height={24} />
+        <Input label="Password" />
+        <Gap height={40} />
+        <Button title="Continue" />
+      </View>
     </View>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    padding: 40,
+    paddingTop: 0,
+  },
+  page: {
+    backgroundColor: colors.white,
+    flex: 1,
+  },
+});
