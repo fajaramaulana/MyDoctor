6b32e52aec4e378f9d9cb4e97d149cd8db93e1c3
fajaramaulana <59821005+fajaramaulana@users.noreply.github.com>
2021-05-02 21:16:12 +0700

    setup dan install firebase di react native

---
+++ src/config/Fire/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDewPXs3u788kY4erlyV7tgVn3yVP2kKpA',
+  authDomain: 'my-doctor-bwa-01.firebaseapp.com',
+  projectId: 'my-doctor-bwa-01',
+  storageBucket: 'my-doctor-bwa-01.appspot.com',
+  messagingSenderId: '84971546188',
+  appId: '1:84971546188:web:986a132be32610468d36e9',
+});
+
+const Fire = firebase;
+
+export default Fire;
